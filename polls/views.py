61af19307e813afb62df0f839374f4eda23d845e
Savitha Nair <savnair36@gmail.com>
2024-02-29 11:12:47 -0500

    requirement and removed unused imports

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
-from django.template import loader
 from django.urls import reverse
 from django.views import generic
 
